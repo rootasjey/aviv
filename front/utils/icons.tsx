353936c743c4b64d2cea4a205b6244c2625924bc
rootasjey <jeremie@rootasjey.dev>
2022-10-03 23:44:00 +0200

    feat: :lipstick: handle sms message type icon

--- front/utils/icons.tsx
+++ front/utils/icons.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import MailIcon from '@mui/icons-material/Mail';
 import PhoneIcon from '@mui/icons-material/Phone';
 import DraftsIcon from '@mui/icons-material/Drafts';
+import SmsFailedIcon from '@mui/icons-material/SmsFailed';
+import SmsIcon from '@mui/icons-material/Sms';
 
 export const getIcon = (type: string, read: boolean) => {
   switch (type) {
@@ -10,9 +12,14 @@ export const getIcon = (type: string, read: boolean) => {
       return read 
         ? <DraftsIcon color={read ? "inherit" : "primary"} />
         : <MailIcon color={read ? "inherit" : "primary"} />;
-  
-    case "phone":
-      return <PhoneIcon color={read ? "inherit" : "primary"} />;
+        
+      case "phone":
+        return <PhoneIcon color={read ? "inherit" : "primary"} />;
+        
+      case "sms":
+        return read 
+          ? <SmsIcon color={read ? "inherit" : "primary"} />
+          : <SmsFailedIcon color={read ? "inherit" : "primary"} />;
       
     default:
       return <MailIcon color={read ? "inherit" : "primary"} />;
